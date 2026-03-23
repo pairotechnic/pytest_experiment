2ec797e856ddfa315a8f6e78b6357ae2fb62402d
Rohit Pai <rohitkalsankpai@gmail.com>
2026-03-23 18:30:29 +0530

    Explored fixtures that execute before each test, for setup purposes, also wrote tests for each scenario of the class UserManager's methods

--- test_main.py
+++ test_main.py
@@ -1,6 +1,28 @@
 
-from main import get_weather, add, divide
+from main import get_weather, add, divide, UserManager
 import pytest
+
+@pytest.fixture
+def user_manager():
+    """Creates a fresh instance of UserManager before each test."""
+    return UserManager()
+
+def test_add_user(user_manager):
+    assert user_manager.add_user("john_doe", "john@example.com") == True
+    assert user_manager.get_user("john_doe") == "john@example.com"
+
+def test_add_duplicate_user(user_manager):
+    user_manager.add_user("john_doe", "john@example.com")
+    with pytest.raises(ValueError, match="User already exists"):
+        user_manager.add_user("john_doe", "another@example.com")
+    assert user_manager.get_user("john_doe") == "john@example.com"
+
+def test_get_nonexistant_user(user_manager):
+    assert user_manager.get_user("john_doe") == None
+
+def test_get_existing_user(user_manager):
+    assert user_manager.add_user("john_doe", "john@example.com") == True
+    assert user_manager.get_user("john_doe") == "john@example.com"
 
 def test_get_weather():
     assert get_weather(15) == "cold"

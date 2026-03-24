d5c51c0d30f11002be6082c2bff465d84a2f49f6
Rohit Pai <rohitkalsankpai@gmail.com>
2026-03-24 11:38:59 +0530

    Implemented parameterized tests, i.e., same test code is executed with various inputs, and compared against exected outputs

---
+++ tests/test_parameterized.py
@@ -0,0 +1,15 @@
+import pytest
+from scripts.parameterized import is_prime
+
+@pytest.mark.parametrize("num, expected", [
+    (1, False),
+    (2, True),
+    (3, True),
+    (4, False),
+    (17, True),
+    (18, False),
+    (19, True),
+    (25, False),
+])
+def test_is_prime(num, expected):
+    assert is_prime(num) == expected
